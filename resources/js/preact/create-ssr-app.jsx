1dcb7c293e81c5d837d1589fc0a1ce3aaf6574e0
Joshua Anderton <info@joshanderton.com>
2022-11-03 23:20:54 -0700

    Updates for blazervel maste

--- resources/js/preact/create-ssr-app.jsx
+++ resources/js/preact/create-ssr-app.jsx
@@ -1,34 +1,6 @@
 import render from '@pckg/preact-render-to-string/jsx'
 import { h } from '@pckg/preact'
-import blazervel from '../actionsjs/app/boot'
-import route from '../actionsjs/app/routes'
-import lang from '../actionsjs/app/translations'
 
 export default async function () {
-  
-  const page = blazervel.loadPage()
-
-  return (
-    render(
-      <>
-        {/* <!DOCTYPE html/> */}
-        <html lang="en" class="dark:bg-black">
-          <head>
-            <meta charset="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <title>Blazervel</title>
-          </head>
-          <body class="font-sans antialiased">
-            <page.component
-              $b={blazervel}
-              route={route}
-              lang={lang}
-              {...page.props}
-            />
-          </body>
-        </html>
-      </>
-    )
-  )
-
+  return render(<></>)
 }
