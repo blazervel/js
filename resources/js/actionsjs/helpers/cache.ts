f67a716cef97143b41a6cff8fdbed28dfbc62e9c
Joshua Anderton <info@joshanderton.com>
2022-11-09 22:48:00 -0800

    Added cache again

--- resources/js/actionsjs/helpers/cache.ts
+++ resources/js/actionsjs/helpers/cache.ts
@@ -1,4 +1,4 @@
-import { setupCache } from '@pckg/axios-cache-adapter/src/api'
+import { setupCache } from '@pckg/axios-cache-adapter'
 import { md5 } from './utils'
 
 export const cacheKey = (url: string, options: object|null) => {
