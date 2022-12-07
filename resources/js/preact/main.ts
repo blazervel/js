f2afc4a798f98bacf8c21a793e0ff2ef0dc9529b
Joshua Anderton <info@joshanderton.com>
2022-12-07 09:31:27 -0800

    Remove tailwind from blazervel base app

--- resources/js/preact/main.ts
+++ resources/js/preact/main.ts
@@ -2,6 +2,5 @@
 // import { h } from '@pckg/preact'
 import { render, createElement } from '@pckg/preact'
 import App from './app'
-import '../../css/tailwind.css'
 
 render(createElement(App), document.body)
